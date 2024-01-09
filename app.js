e1ad893cd2c40ea5418b92d045d02eb2570dc83c
Caylin Vu <caylinvu@gmail.com>
2024-01-09 12:28:41 -0600

    Set up mongoose connection

--- app.js
+++ app.js
@@ -16,6 +16,16 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+// mongoose connection setup
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const mongoDB = process.env.MONGODB_URI || process.env.dev_db_url;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
