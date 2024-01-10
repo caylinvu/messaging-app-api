e3872dc5ada162fb9f2c07abf6e5da147febaa67
Caylin Vu <caylinvu@gmail.com>
2024-01-10 16:29:59 -0600

    Update data models

--- models/conversation.js
+++ models/conversation.js
@@ -6,6 +6,7 @@ const ConversationSchema = new Schema({
   members: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
   exclude: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   isGroup: { type: Boolean, default: false, required: true },
+  lastMessage: { type: Schema.Types.ObjectId, ref: 'Message' },
   image: { type: String },
   groupName: { type: String },
 });
