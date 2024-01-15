87960e062f8cf4a62305bf498a977a6238607cd3
Caylin Vu <caylinvu@gmail.com>
2024-01-15 17:29:49 -0600

    Handle recieving a new message through socket

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -13,8 +13,8 @@ exports.getAllMessages = asyncHandler(async (req, res, next) => {
 // Display all messages from a specific conversation
 exports.getMessages = asyncHandler(async (req, res, next) => {
   const messagesInConversation = await Message.find({ conversation: req.params.conversationId })
-    .populate('conversation', '-lastMessage -timestamp')
-    .populate('author', '-email -password')
+    // .populate('conversation', '-lastMessage -timestamp')
+    .populate('author', 'firstName lastName')
     .sort({ timestamp: 1 })
     .exec();
   return res.send(messagesInConversation);
--- socketApi.js
+++ socketApi.js
@@ -1,16 +1,49 @@
+const asyncHandler = require('express-async-handler');
 const io = require('socket.io')();
 const socketApi = {
   io: io,
 };
 
-// Add your socket.io logic here!
+const Conversation = require('./models/conversation');
+const Message = require('./models/message');
+
+// Socket connection
 io.on('connection', (socket) => {
   console.log('A user connected');
+
+  // Socket disconnection
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
+
+  // Listen for a new message
+  socket.on(
+    'message',
+    asyncHandler(async (msg) => {
+      console.log('Message received by server');
+      console.log(msg);
+
+      // Save message to database
+      const message = new Message({
+        text: msg.text,
+        author: msg.author,
+        conversation: msg.conversation,
+        image: msg.image,
+      });
+      await message.save();
+
+      // Save message as last message in conversation
+      await Conversation.findByIdAndUpdate(
+        msg.conversation,
+        { $set: { lastMessage: message._id } },
+        {},
+      );
+
+      // Emit message to sockets
+      io.emit('message', msg);
+    }),
+  );
 });
-// end of socket.io logic
 
 module.exports = socketApi;
 
