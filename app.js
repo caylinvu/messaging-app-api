d842e71561d49fc3298a6c43c6682709f9636166
Caylin Vu <caylinvu@gmail.com>
2024-04-08 16:36:11 -0500

    Add origin variable

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 // cors setup
 const cors = require('cors');
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+  origin: ['http://localhost:5173', process.env.ORIGIN],
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
